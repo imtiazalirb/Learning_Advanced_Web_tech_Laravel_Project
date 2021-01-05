2d4f7fc15d13ec3b02200f46e1dc5cf6efd3bf3f
Sabbir <sbbrhs@gmail.com>
2021-01-05 12:04:03 +0600

    fix api route

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('delivery', 'Delivery/Delivery');
+Route::apiResource('delivery', 'Delivery/Delivery', array("as" => "api"));
